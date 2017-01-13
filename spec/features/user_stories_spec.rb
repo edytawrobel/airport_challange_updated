9412955b63f7837512a78e399df69b7f947ba219
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-01-13 22:15:52 +0000

    US3: raise error when airport full

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -3,7 +3,7 @@ describe 'these are the user stories' do
   # So I can get passengers to a destination
   # I want to instruct a plane to land at an airport and confirm that it has landed
   it 'so planes land at airports, instruct a plane to land' do
-    airport = Airport.new
+    airport = Airport.new(20)
     plane = Plane.new
     expect { airport.land(plane) }.not_to raise_error
   end
@@ -12,10 +12,34 @@ describe 'these are the user stories' do
 #   So I can get passengers on the way to their destination
 #   I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
   it 'so planes take off from airports, instruct a plane to take off' do
-    airport = Airport.new
+    airport = Airport.new(20)
     plane = Plane.new
     expect { airport.take_off(plane) }.not_to raise_error
   end
 
+  # As an air traffic controller
+  # To ensure safety
+  # I want to prevent landing when the airport is full
+  it 'does not allow planes to land when airport is full' do
+    airport = Airport.new(20)
+    plane = Plane.new
+    20.times do
+      airport.land(plane)
+    end
+    expect { airport.land(plane) }.to raise_error 'Cannot land plane: airport full'
+  end
+
+
 
 end
+
+
+
+
+
+
+
+
+  # As an air traffic controller
+  # To ensure safety
+  # I want to prevent takeoff when weather is stormy
